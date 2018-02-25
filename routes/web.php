05b362f5c341531b8238213a46f74eaf11eedbe2
shahriarcse <cse.shahriar.hosen@gmail.com>
2018-02-25 12:16:44 +0600

    admin panel templating done

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ Route::get('/', function () {
     return view('user.blog');     
 });
 
-Route::get('post', function () { 
+Route::get('post', function () {  
     return view('user.post');    
 })->name('post');    
+
+Route::get('admin/home', function () { 
+    return view('admin.home');    
+})->name('home');     
